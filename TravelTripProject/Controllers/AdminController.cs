66809da32f429dfa6ae0424a183eca04a1344a35
Fatih Kürekçi <fatihkr02@gmail.com>
2024-09-20 13:21:07 +0300

    added admin template & created blog processes

---
+++ TravelTripProject/Controllers/AdminController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProject.Models.Entities;
+
+namespace TravelTripProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        Context context = new Context();
+
+        public ActionResult Index()
+        {
+            var values = context.Blogs.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult NewBlog()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewBlog(Blog blog)
+        {
+            context.Blogs.Add(blog);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteBlog(int id)
+        {
+            var blog = context.Blogs.Find(id);
+            context.Blogs.Remove(blog);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult GetBlog(int id)
+        {
+            var blog = context.Blogs.Find(id);
+            return View("GetBlog", blog);
+        }
+
+        public ActionResult EditBlog(Blog b)
+        {
+            var blg = context.Blogs.Find(b.BlogID);
+            blg.Description = b.Description;
+            blg.Title = b.Title;
+            blg.ImageURL = b.ImageURL;
+            blg.CreatedAt = b.CreatedAt;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
